fa9064f86a6eff1dbf6e81117ec3f995033ce58e
Igor Ferreira <ferreiraigor007@gmail.com>
2026-06-12 23:58:35 -0300

    Ajuda do Armando

---
+++ src/InventoryControl.Application/interfaces/IProductService.cs
@@ -0,0 +1,6 @@
+﻿namespace InventoryControl.Application.interfaces;
+
+public interface IProductService
+{
+    Task CreateProduct(string name, string? description, decimal price, int stockQuantity);
+}
