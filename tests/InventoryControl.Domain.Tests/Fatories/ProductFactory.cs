27388122b98d6b167dfcaf78d85e73e06f20215b
Igor Ferreira <ferreiraigor007@gmail.com>
2026-06-14 00:47:52 -0300

    Product factories created

---
+++ tests/InventoryControl.Domain.Tests/Fatories/ProductFactory.cs
@@ -0,0 +1,11 @@
+﻿using InventoryControl.Domain.Entities;
+
+namespace InventoryControl.Domain.Tests.Fatories;
+
+public static class ProductFactory
+{
+    public static Product Create(string name = "Test Product", decimal price = 10.99m, string? description = "This is a test product.")
+    {
+        return Product.Create(name, price, description);
+    }
+}
