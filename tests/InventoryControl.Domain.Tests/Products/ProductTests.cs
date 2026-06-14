ce2f3cdae0c12aa574558916e0ba708c37da89b4
Igor Ferreira <ferreiraigor007@gmail.com>
2026-06-14 00:49:23 -0300

    ProductTests has been refactored

--- tests/InventoryControl.Domain.Tests/Products/ProductTests.cs
+++ tests/InventoryControl.Domain.Tests/Products/ProductTests.cs
@@ -1,5 +1,6 @@
 ﻿using InventoryControl.Domain.Entities;
 using InventoryControl.Domain.Exceptions;
+using InventoryControl.Domain.Tests.Fatories;
 
 namespace InventoryControl.Domain.Tests.Products;
 
@@ -13,7 +14,7 @@ public class ProductTests
         string description = "This is a test product.";
         decimal price = 10.99m;
         // Act
-        var product = Product.Create(name, description, price);
+        var product = Product.Create(name, price, description);
         // Assert
         Assert.Equal(name, product.Name);
         Assert.Equal(description, product.Description);
@@ -24,130 +25,107 @@ public class ProductTests
     [Fact]
     public void Should_Throw_When_Name_Is_Empty()
     {
-        // Arrange
-        string name = "";
-        decimal price = 10.99m;
-        // Act & Assert
-        Assert.Throws<DomainException>(() => Product.Create(name, null, price));
+        Assert.Throws<DomainException>(() => ProductFactory.Create(name: ""));
     }
 
     [Fact]
     public void Should_Throw_When_Changing_Name_To_Empty()
     {
-        // Arrange
-        var product = Product.Create("Test Product", null, 10.99m);
-        // Act & Assert
+        var product = ProductFactory.Create();
+
         Assert.Throws<DomainException>(() => product.ChangeName(""));
     }
 
     [Fact]
     public void Should_Throw_When_Price_Is_Invalid()
     {
-        // Arrange
-        string name = "Test Product";
-        decimal price = -5.00m;
-        // Act & Assert
-        Assert.Throws<DomainException>(() => Product.Create(name, null, price));
+        Assert.Throws<DomainException>(() => ProductFactory.Create(price: -5));
     }
 
     [Fact]
     public void Should_Throw_When_Changing_Price_To_Invalid_Value()
     {
-        // Arrange
-        var product = Product.Create("Test Product", null, 10.99m);
-        // Act & Assert
+        var product = ProductFactory.Create();
+
         Assert.Throws<DomainException>(() => product.ChangePrice(0));
     }
 
     [Fact]
     public void Should_Increase_Stock()
     {
-        // Arrange
-        var product = Product.Create("Test Product", null, 10.99m);
-        int quantityToAdd = 10;
-        // Act
-        product.IncreaseStock(quantityToAdd);
-        // Assert
-        Assert.Equal(quantityToAdd, product.StockQuantity);
+        var product = ProductFactory.Create();
+
+        product.IncreaseStock(10);
+
+        Assert.Equal(10, product.StockQuantity);
     }
 
     [Fact]
-    public void Should_Throws_When_Increase_Invalid_Quantity()
+    public void Should_Throw_When_Increase_Invalid_Quantity()
     {
-        // Arrange
-        var product = Product.Create("Test Product", null, 10.99m);
-        int quantityToAdd = 0;
-        // Act & Assert
-        Assert.Throws<DomainException>(() => product.IncreaseStock(quantityToAdd));
+        var product = ProductFactory.Create();
+        
+        Assert.Throws<DomainException>(() => product.IncreaseStock(0));
     }
 
     [Fact]
     public void Should_Decrease_Stock()
     {
-        // Arrange
-        var product = Product.Create("Test Product", null, 10.99m);
+        var product = ProductFactory.Create();
+
         product.IncreaseStock(20);
-        int quantityToRemove = 5;
-        // Act
-        product.DecreaseStock(quantityToRemove);
-        // Assert
+        product.DecreaseStock(5);
+        
         Assert.Equal(15, product.StockQuantity);
     }
 
     [Fact]
-    public void Should_Throws_When_Stock_Is_Insufficient()
+    public void Should_Throw_When_Stock_Is_Insufficient()
     {
-        // Arrange
-        var product = Product.Create("Test Product", null, 10.99m);
+        var product = ProductFactory.Create();
+
         product.IncreaseStock(5);
-        int quantityToRemove = 10;
-        // Act & Assert
-        Assert.Throws<DomainException>(() => product.DecreaseStock(quantityToRemove));
+
+        Assert.Throws<DomainException>(() => product.DecreaseStock(10));
     }
 
     [Fact]
-    public void Should_Throws_When_Decrease_Invalid_Quantity()
+    public void Should_Throw_When_Decrease_Invalid_Quantity()
     {
-        // Arrange
-        var product = Product.Create("Test Product", null, 10.99m);
+        var product = ProductFactory.Create();
+
         product.IncreaseStock(5);
-        int quantityToRemove = -5;
-        // Act & Assert
-        Assert.Throws<DomainException>(() => product.DecreaseStock(quantityToRemove));
+
+        Assert.Throws<DomainException>(() => product.DecreaseStock(-5));
     }
 
     [Fact]
     public void Should_Change_Product_Name()
     {
-        // Arrange
-        var product = Product.Create("Test Product", null, 10.99m);
-        string newName = "Updated Product Name";
-        // Act
-        product.ChangeName(newName);
-        // Assert
-        Assert.Equal(newName, product.Name);
+        var product = ProductFactory.Create(name: "Test Product");
+
+        product.ChangeName("Updated Product Name");
+
+        Assert.Equal("Updated Product Name", product.Name);
     }
 
     [Fact]
     public void Should_Change_Product_Price()
     {
-        // Arrange
-        var product = Product.Create("Test Product", null, 10.99m);
-        decimal newPrice = 5.58m;
-        // Act
-        product.ChangePrice(newPrice);
-        // Assert
-        Assert.Equal(newPrice, product.Price);
+        var product = ProductFactory.Create(price: 10.99m);
+
+        product.ChangePrice(5.58m);
+
+        Assert.Equal(5.58m, product.Price);
     }
 
     [Fact]
-    public void Should_Set_CreatedAt_On_Creation()
+    public void Should_Validate_The_Created_At()
     {
-        // Arrange
         var before = DateTime.UtcNow;
-        // Act
-        var product = Product.Create("Test Product", null, 10.99m);
-        // Assert
+
+        var product = ProductFactory.Create();
+
         Assert.True(product.CreatedAt >= before);
     }
 }
