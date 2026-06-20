b1a41d5382142284273a318a341ca62298e51ba3
Igor Ferreira <ferreiraigor007@gmail.com>
2026-06-20 19:51:45 -0300

    DTO factory created for product

---
+++ tests/InventoryControl.Application.Tests/Factories/CreateProductRequestDtoFactory.cs
@@ -0,0 +1,11 @@
+﻿using InventoryControl.Application.DTOs;
+
+namespace InventoryControl.Application.Tests.Factories;
+
+public static class CreateProductRequestDtoFactory
+{
+    public static CreateProductRequestDto Create(string name = "Test Product", decimal price = 10.99m, string? description = "This is a test product.")
+    {
+        return new CreateProductRequestDto(name, price, description);
+    }
+}
