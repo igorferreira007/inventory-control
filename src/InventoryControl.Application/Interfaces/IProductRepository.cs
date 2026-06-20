c66bce36bae9cdad8259a11cbc2ff41f3f40a1a1
Igor Ferreira <ferreiraigor007@gmail.com>
2026-06-20 19:42:22 -0300

    The product repository interface has been created.

---
+++ src/InventoryControl.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using InventoryControl.Domain.Entities;
+
+namespace InventoryControl.Application.Interfaces;
+
+public interface IProductRepository
+{
+    Task CreateAsync(Product product);
+    Task<Product?> FindByIdAsync(long id);
+    Task<Product?> FindByNameAsync(string name);
+    Task<Product[]> FindManyAsync(int pageNumber);
+    Task SaveAsync(Product product);
+    Task DeleteAsync(Product product);
+}
