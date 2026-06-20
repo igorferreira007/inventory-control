335158af0df237a6abb4dd880f4c00387fad3f7c
Igor Ferreira <ferreiraigor007@gmail.com>
2026-06-20 19:48:52 -0300

    The in-memory product repository has been implemented.

---
+++ tests/InventoryControl.Application.Tests/Repositories/InMemoryProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using InventoryControl.Application.Interfaces;
+using InventoryControl.Domain.Entities;
+
+namespace InventoryControl.Application.Tests.Repositories;
+
+public class InMemoryProductRepository : IProductRepository
+{
+    public List<Product> Products { get; } = [];
+
+    public Task CreateAsync(Product product)
+    {
+        Products.Add(product);
+
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product?> FindByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product?> FindByNameAsync(string name)
+    {
+        var product = Products.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+        return Task.FromResult(product);
+    }
+
+    public Task<Product[]> FindManyAsync(int pageNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+}
