4745e08952852602eef91aeb86e746bb73d6e10a
Igor Ferreira <ferreiraigor007@gmail.com>
2026-06-27 12:25:18 -0300

    Created the product factory

---
+++ tests/InventoryControl.Application.Tests/Factories/ProductFactory.cs
@@ -0,0 +1,26 @@
+﻿using InventoryControl.Domain.Entities;
+
+namespace InventoryControl.Application.Tests.Factories;
+
+public class ProductFactory
+{
+    public static Product Create(
+        long id = 1,
+        string? name = null,
+        string? description = null,
+        decimal price = 10.99m,
+        int stockQuantity = 10,
+        DateTime? createdAt = null)
+    {
+        name ??= $"Product test {id}";
+        description ??= $"Product description test {id}.";
+
+        return Product.Restore(
+            id,
+            name,
+            price,
+            stockQuantity,
+            createdAt ?? DateTime.UtcNow,
+            description);
+    }
+}
