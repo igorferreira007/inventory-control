7ce33da0c5d8caea1152d6d6eb1afd7d87ec6298
Igor Ferreira <ferreiraigor007@gmail.com>
2026-06-27 12:24:32 -0300

    Created the ProductResponseDto

---
+++ src/InventoryControl.Application/DTOs/ProductResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryControl.Application.DTOs;
+
+public record ProductResponseDto(
+    long Id,
+    string Name,
+    decimal Price,
+    int StockQuantity,
+    string? Description,
+    DateTime CreatedAt);
