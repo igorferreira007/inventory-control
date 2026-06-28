6884889181557bab5e1f103ba4955df467f8cf3c
Igor Ferreira <ferreiraigor007@gmail.com>
2026-06-28 20:20:01 -0300

    Add increase product stock use case

---
+++ src/InventoryControl.Application/Validators/IncreaseProductStockRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using InventoryControl.Application.DTOs;
+using InventoryControl.Application.Resources;
+
+namespace InventoryControl.Application.Validators;
+
+public class IncreaseProductStockRequestValidator : AbstractValidator<IncreaseProductStockRequestDto>
+{
+    public IncreaseProductStockRequestValidator()
+    {
+        RuleFor(p => p.ProductId).GreaterThan(0).WithMessage(ResourceMessages.PRODUCT_ID_INVALID);
+        RuleFor(p => p.Quantity).GreaterThan(0).WithMessage(ResourceMessages.QUANTITY_GREATER_THAN_ZERO);
+    }
+}
