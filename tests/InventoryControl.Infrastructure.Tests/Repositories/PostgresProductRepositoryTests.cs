1ba83911f161dc5bcc4bec066091ead9e1c53cbb
Igor Ferreira <ferreiraigor007@gmail.com>
2026-06-30 20:07:25 -0300

    Implementation of the PostgreSQL product repository completed.

--- tests/InventoryControl.Infrastructure.Tests/Repositories/PostgresProductRepositoryTests.cs
+++ tests/InventoryControl.Infrastructure.Tests/Repositories/PostgresProductRepositoryTests.cs
@@ -205,6 +205,109 @@ public class PostgresProductRepositoryTests
         Assert.Equal("Product 22", products[1].Name);
     }
 
+    [Fact]
+    public async Task Should_Save_Product()
+    {
+        await ClearDatabaseAsync();
+
+        var product = Product.Create(
+            name: "Product test",
+            price: 10.99m,
+            description: "Product description test.");
+
+        await _repository.CreateAsync(product);
+
+        product.ChangeName("Product changed");
+        product.ChangePrice(15.99m);
+        product.ChangeDescription("Product description changed.");
+        product.IncreaseStock(10);
+
+        await _repository.SaveAsync(product);
+
+        var productFound = await _repository.FindByIdAsync(product.Id);
+
+        Assert.NotNull(productFound);
+        Assert.Equal(product.Id, productFound.Id);
+        Assert.Equal("Product changed", productFound.Name);
+        Assert.Equal(15.99m, productFound.Price);
+        Assert.Equal("Product description changed.", productFound.Description);
+        Assert.Equal(10, productFound.StockQuantity);
+    }
+
+    [Fact]
+    public async Task Should_Not_Update_CreatedAt_When_Saving_Product()
+    {
+        await ClearDatabaseAsync();
+
+        var product = Product.Create(
+            name: "Product test",
+            price: 10.99m,
+            description: "Product description test.");
+
+        await _repository.CreateAsync(product);
+
+        var productBeforeUpdate = await _repository.FindByIdAsync(product.Id);
+
+        Assert.NotNull(productBeforeUpdate);
+
+        product.ChangeName("Product changed");
+
+        await _repository.SaveAsync(product);
+
+        var productAfterUpdate = await _repository.FindByIdAsync(product.Id);
+
+        Assert.NotNull(productAfterUpdate);
+        Assert.Equal(productBeforeUpdate.CreatedAt, productAfterUpdate.CreatedAt);
+    }
+
+    [Fact]
+    public async Task Should_Delete_Product()
+    {
+        await ClearDatabaseAsync();
+
+        var product = Product.Create(
+            name: "Product test",
+            price: 10.99m,
+            description: "Product description test.");
+
+        await _repository.CreateAsync(product);
+
+        var productBeforeDelete = await _repository.FindByIdAsync(product.Id);
+
+        Assert.NotNull(productBeforeDelete);
+
+        await _repository.DeleteAsync(product);
+
+        var productAfterDelete = await _repository.FindByIdAsync(product.Id);
+
+        Assert.Null(productAfterDelete);
+    }
+
+    [Fact]
+    public async Task Should_Delete_Only_Selected_Product()
+    {
+        await ClearDatabaseAsync();
+
+        var product1 = Product.Create(
+            name: "Product test 1",
+            price: 10.99m);
+
+        var product2 = Product.Create(
+            name: "Product test 2",
+            price: 15.99m);
+
+        await _repository.CreateAsync(product1);
+        await _repository.CreateAsync(product2);
+
+        await _repository.DeleteAsync(product2);
+
+        var product1Found = await _repository.FindByIdAsync(product1.Id);
+        var product2Found = await _repository.FindByIdAsync(product2.Id);
+
+        Assert.NotNull(product1Found);
+        Assert.Null(product2Found);
+    }
+
     private async Task ClearDatabaseAsync()
     {
         await using var connection = _connectionFactory.CreateConnection();
